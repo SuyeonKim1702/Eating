8412ec4b0beeb9b8bb905b51dda066e3ffe2fe65
eunjin <lejerin1112@gmail.com>
2021-03-14 17:58:59 +0900

    Recyclerview ViewType별 수정 완료

--- Eating_aos/app/src/main/java/desla/aos/eating/ui/like/LikeFragment.kt
+++ Eating_aos/app/src/main/java/desla/aos/eating/ui/like/LikeFragment.kt
@@ -1,12 +1,17 @@
 package desla.aos.eating.ui.like
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import desla.aos.eating.R
+import desla.aos.eating.data.model.Post
 import desla.aos.eating.data.repositories.HomeRepository
 import desla.aos.eating.data.repositories.LikeRepository
 import desla.aos.eating.data.repositories.UserRepository
 import desla.aos.eating.databinding.FragmentHomeBinding
 import desla.aos.eating.ui.base.BaseFragment
+import desla.aos.eating.ui.home.HomeRCAdapter
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_like.*
 
 class LikeFragment :  BaseFragment<FragmentHomeBinding>() {
 
@@ -17,6 +22,9 @@ class LikeFragment :  BaseFragment<FragmentHomeBinding>() {
     private lateinit var viewModel: LikeViewModel
 
     private val TAG = "HomeFragment"
+
+    private val chat: MutableList<Post> = mutableListOf()
+    private val like: MutableList<Post> = mutableListOf()
 
     override fun initStartView() {
         val repository = LikeRepository()
@@ -30,8 +38,24 @@ class LikeFragment :  BaseFragment<FragmentHomeBinding>() {
     }
 
     override fun initAfterBinding() {
+        chat.add(Post(5, 100, "공차 같이 드실 분1"))
+        chat.add(Post(6, 100, "신전 같이 드실 분2"))
+        chat.add(Post(7, 100, "엽떡 같이 드실 분3"))
+        chat.add(Post(8, 100, "치킨 같이 드실 분4"))
 
+        like.add(Post(5, 100, "공차 같이 드실 분1"))
+        like.add(Post(6, 100, "신전 같이 드실 분2"))
+        like.add(Post(7, 100, "엽떡 같이 드실 분3"))
+        like.add(Post(8, 100, "치킨 같이 드실 분4"))
+
+        initRc()
     }
 
+    private fun initRc(){
+        rc_chat.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        rc_chat.setHasFixedSize(true)
+        rc_chat.adapter = LikeRCAdapter(chat, like)
+
+    }
 
 }
---
+++ Eating_aos/app/src/main/java/desla/aos/eating/ui/like/LikeRCAdapter.kt
@@ -0,0 +1,132 @@
+package desla.aos.eating.ui.like
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import desla.aos.eating.R
+import desla.aos.eating.data.model.Post
+import desla.aos.eating.databinding.RcHomeBinding
+
+class LikeRCAdapter (
+        private val chatList : List<Post>,
+        private val likeList : List<Post>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    companion object
+    {
+        private const val VIEW_TYPE_TITLE_CHAT = 0
+        private const val VIEW_TYPE_CHAT_DATA = 1
+        private const val VIEW_TYPE_TITLE_LIKE = 2
+        private const val VIEW_TYPE_LIKE_DATA = 3
+        private const val VIEW_TYPE_BOTTOM = 4
+    }
+
+
+    override fun getItemCount() = if (chatList.isEmpty()) likeList.size + 2 else chatList.size + likeList.size + 3
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = when (viewType)
+    {
+        VIEW_TYPE_TITLE_CHAT ->
+        {//inflates progressbar layout
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.like_chat_title,parent,false)
+            ChatTitleViewHolder(view)
+        }
+
+        VIEW_TYPE_CHAT_DATA ->
+        {//inflates row layout
+            ChatViewHolder(
+                    DataBindingUtil.inflate(
+                            LayoutInflater.from(parent.context),
+                            R.layout.rc_home,
+                            parent,
+                            false
+                    )
+            )
+        }
+
+        VIEW_TYPE_TITLE_LIKE ->
+        {//inflates progressbar layout
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.like_title,parent,false)
+            LikeTitleViewHolder(view)
+        }
+
+        VIEW_TYPE_LIKE_DATA ->
+        {//inflates row layout
+            LikeViewHolder(
+                    DataBindingUtil.inflate(
+                            LayoutInflater.from(parent.context),
+                            R.layout.rc_home,
+                            parent,
+                            false
+                    )
+            )
+        }
+
+        VIEW_TYPE_BOTTOM ->
+        {//inflates progressbar layout
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.bottom,parent,false)
+            BottomViewHolder(view)
+        }
+        else -> throw IllegalArgumentException("Different View type")
+    }
+
+    override fun getItemViewType(position: Int): Int
+    {
+
+        if(chatList.isEmpty()){
+            if(position == 0) return VIEW_TYPE_TITLE_LIKE
+            else if(position <= likeList.size) return VIEW_TYPE_LIKE_DATA
+        }else{
+            //비어있지 않다면
+            if(position == 0) return VIEW_TYPE_TITLE_CHAT
+            else if(position < chatList.size + 1) return VIEW_TYPE_CHAT_DATA
+            else if(position == chatList.size + 1) return VIEW_TYPE_TITLE_LIKE
+            else if(position <= chatList.size + likeList.size + 1) return VIEW_TYPE_LIKE_DATA
+        }
+
+
+        return VIEW_TYPE_BOTTOM
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+        if (holder is ChatViewHolder)
+        {
+            holder.homeBinding.post = chatList[position-1]
+        }
+
+        if (holder is LikeViewHolder)
+        {
+            if(chatList.isEmpty()){
+                holder.homeBinding.post = likeList[position-1]
+            }else{
+                holder.homeBinding.post = likeList[position-chatList.size-2]
+            }
+
+        }
+
+    }
+
+    inner class ChatViewHolder(
+            val homeBinding: RcHomeBinding
+    ) : RecyclerView.ViewHolder(homeBinding.root)
+
+    inner class LikeViewHolder(
+            val homeBinding: RcHomeBinding
+    ) : RecyclerView.ViewHolder(homeBinding.root)
+
+    inner class ChatTitleViewHolder(
+            itemView: View
+    ) : RecyclerView.ViewHolder(itemView)
+
+    inner class LikeTitleViewHolder(
+            itemView: View
+    ) : RecyclerView.ViewHolder(itemView)
+
+    inner class BottomViewHolder(
+            itemView: View
+    ) : RecyclerView.ViewHolder(itemView)
+
+}
